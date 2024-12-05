1a66d4bec59ac0d57e281a6d6a050cc661fb50bb
kangaroo <tgkhang22@clc.fitus.edu.vn>
2024-12-05 23:25:25 +0700

    new config

--- index.js
+++ index.js
@@ -29,8 +29,10 @@ app.engine(
 );
 app.set("view engine", "hbs");
 
+
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
 
 app.get("/", (req, res) => res.redirect("/users"));
 app.use("/users", require("./routes/userRouter"));
